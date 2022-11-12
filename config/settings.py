8637abb83205b565b97f83dd20012f8cfaca6b16
Arvin Visi <arvinvisi03@gmail.com>
2022-11-12 13:52:36 +0330

    add model in products

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    active = models.BooleanField(default=False)
+
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
