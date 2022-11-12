6aecac8330560d1dfe87acce412b0acfb3c428ab
Arvin Visi <arvinvisi03@gmail.com>
2022-11-12 14:11:04 +0330

    add ProductListView and edit models

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField
+    title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.PositiveIntegerField()
     active = models.BooleanField(default=False)
