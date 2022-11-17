8d6f7456c1491b6a1805b90ebfc98f97cb30de32
Arvin Visi <arvinvisi03@gmail.com>
2022-11-17 18:14:49 +0330

    add pagination in ProductListView

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'products/product_list.html'
     context_object_name = 'products'
+    paginate_by = 1
 
 
 class ProductDetailView(generic.DetailView):
