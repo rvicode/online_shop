29fedb086d44c6ea8a828df9c17250b1f4ce137d
Arvin Visi <arvinvisi03@gmail.com>
2022-11-20 21:53:22 +0330

    add translate in project

--- config/settings.py
+++ config/settings.py
@@ -130,12 +130,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
