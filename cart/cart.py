1c4bf4c1293a6ee09167447af8ca044816a98db2
Arvin Visi <arvinvisi03@gmail.com>
2022-12-03 19:39:21 +0330

    add cart.py and source

---
+++ cart/cart.py
@@ -0,0 +1,45 @@
+
+class Cart:
+    def __init__(self, request):
+        """
+        Initialize the cart
+        """
+        self.request = request
+
+        self.session = request.session
+
+        cart = self.session.get('cart')
+
+        if not cart:
+            cart = self.session['cart'] = {}
+
+        self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+        Add the specified product to the cart if it exists
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def remove(self, product):
+        """
+        Remove a product from the cart
+        """
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+        """
+        Mark session as modified to save changes
+        """
+        self.session.modified = True
