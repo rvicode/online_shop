60ea71330658c8b1622dfe58c9c6bada730929a9
Arvin Visi <arvinvisi03@gmail.com>
2022-12-05 16:24:40 +0330

    add add_to_cart_view

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import  path
+
+from . import views
+
+
+app_name = 'cart'
+urlpatterns = [
+    path('', views.cart_detail_view, name='cart_detail')
+]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from products.models import Product
+from .forms import ProductCartForm
+from .cart import Cart
+
+
+def cart_detail_view(request):
+    cart = Cart(request)
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
+
+
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = ProductCartForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
