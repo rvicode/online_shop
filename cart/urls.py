e10bb0b81b73c85e8be1a38b80c58e0ebf3d2cd2
Arvin Visi <arvinvisi03@gmail.com>
2022-12-07 19:12:56 +0330

    add remove_to_cart_view

--- cart/urls.py
+++ cart/urls.py
@@ -7,4 +7,5 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail_view, name='cart_detail'),
     path('add/<int:product_id>', views.add_to_cart_view, name='cart_add'),
+    path('remove/<int:product_id>', views.remove_to_cart_view, name='cart_remove'),
 ]
