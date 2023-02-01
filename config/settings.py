d3ec51fe572957d8c38dccd4184b4e402cb6d985
Arvin Visi <arvinvisi03@gmail.com>
2023-02-01 11:57:42 +0330

    add orders app

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
     'persian_translate.apps.PersianTranslateConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 SITE_ID = 1
