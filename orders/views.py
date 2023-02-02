ebe450d545386348657803a7b904d970516c318e
Arvin Visi <arvinvisi03@gmail.com>
2023-02-02 19:41:56 +0330

    make form in order_create_view

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import OrderForm
 
 
 def order_create_view(request):
-    form = OrderForm()
-    return render(request, 'orders/order_create.html', {'form': form})
+    if request.user.is_authenticated:
+        form = OrderForm()  
+        if request.method == 'POST':
+            form = OrderForm(request.POST)
+            if form.is_valid():
+                forms = form.save(commit=False)
+                forms.username = request.user
+                print(forms.username)
+                forms.save()
+                return redirect('product:product_list')
+
+        return render(request, 'orders/order_create.html', {'form': form})
+    else:
+        return redirect('account_login')
