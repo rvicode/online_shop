71e9ddbc49c7b0ad99499584531913e1d2799a4b
Arvin Visi <arvinvisi03@gmail.com>
2023-02-03 11:29:04 +0330

    edit form in order_create.html

--- orders/views.py
+++ orders/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .forms import OrderForm
 
 
+@login_required
 def order_create_view(request):
-    if request.user.is_authenticated:
-        form = OrderForm()  
-        if request.method == 'POST':
-            form = OrderForm(request.POST)
-            if form.is_valid():
-                forms = form.save(commit=False)
-                forms.username = request.user
-                print(forms.username)
-                forms.save()
-                return redirect('product:product_list')
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            forms = form.save(commit=False)
+            forms.username = request.user
+            print(forms.username)
+            forms.save()
+            return redirect('product:product_list')
 
-        return render(request, 'orders/order_create.html', {'form': form})
-    else:
-        return redirect('account_login')
+    return render(request, 'orders/order_create.html', {'form': form})
