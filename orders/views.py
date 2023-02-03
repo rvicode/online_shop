a37a3483fab41e118a41807f87c0b00575f02622
Arvin Visi <arvinvisi03@gmail.com>
2023-02-03 12:12:01 +0330

    create OrderItem in views.py

--- orders/views.py
+++ orders/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
+from .models import OrderItem
 from .forms import OrderForm
+from cart.cart import Cart
 
 
 @login_required
 def order_create_view(request):
     form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('Your Cart Is empty!'))
+        return redirect('product:product_list')
+
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            forms = form.save(commit=False)
-            forms.username = request.user
-            print(forms.username)
-            forms.save()
-            return redirect('product:product_list')
+            order_obj = form.save(commit=False)
+            order_obj.username = request.user
+            print(order_obj.username)
+            order_obj.save()
+
+        for item in cart:
+            product = item['product_obj']
+            OrderItem.objects.create(
+                order=order_obj,
+                product=product,
+                quantity=item['quantity'],
+                price=product.price,
+            )
+
+        cart.clear()
+
+        request.user.first_name = order_obj.firstname
+        request.user.last_name = order_obj.lastname
+        request.user.save()
+
+        messages.success(request, _('Your order has successfully placed.'))
 
     return render(request, 'orders/order_create.html', {'form': form})
