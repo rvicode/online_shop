8169d33ce7b0616c6b6c162a2aaf5e6e0a241efb
Arvin Visi <arvinvisi03@gmail.com>
2023-02-05 15:31:32 +0330

    edit urls.py in config file

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('account/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('orders.urls')),
 
