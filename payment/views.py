11f99ecb737c6dc621edb0ef92b5a30793c2d090
Arvin Visi <arvinvisi03@gmail.com>
2023-02-06 13:26:54 +0330

    add zarinpal request and response in views.py

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+import requests
+import json
+
+from django.shortcuts import get_object_or_404
+from django.conf import settings
 
 from orders.models import Order
 
@@ -10,3 +14,18 @@ def payment_process(request):
     toman_total_price = order.get_total_price()
     rial_total_price = toman_total_price * 10
     
+    zarinpal_request_url = 'https://api.zarinpal.com/pg/v4/payment/request.json'
+
+    request_header = {
+        "accept": "application/json",
+        "content-type": "application/json",
+    }
+
+    request_data = {
+        'merchant_id': settings.ZARINPAL_MERCHANT_ID,
+        'amount': rial_total_price,
+        'description': f"#{order.id} : {order.firstname} {order.lastname}",
+        'callback_url': '127.0.0.1:8000'
+    }
+
+    res = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
